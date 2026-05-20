ef2160ff4a42702dbce6f2f4cf9c25e33871842f
Muhammad Ali Raza <aalirazamughal71@gmail.com>
2026-05-20 21:04:29 +0500

    feat: add Capacitor support and new build scripts for Android

---
+++ task-basic/android/app/src/main/java/com/aliraza/taskbasic/MainActivity.java
@@ -0,0 +1,5 @@
+package com.aliraza.taskbasic;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ task-basic/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aliraza.taskbasic',
+  appName: 'Task Basic',
+  webDir: 'dist/task-basic/browser'
+};
+
+export default config;
